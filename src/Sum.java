59c5c51c1583493e36f549916b92ecaaf1068f8c
lgb211 <rkdqls0122@naver.com>
2022-08-01 20:38:55 +0900

    fix sum file

--- src/Sum.java
+++ src/Sum.java
@@ -2,20 +2,22 @@ import java.util.Scanner;
 
 public class Sum {
 	public static void main(String[] args) {
-		//키보드 두 수의 입력
-		//화면에 두수의 합 출력
-		Scanner scan = new Scanner(System.in);
-		String one =scan.nextLine();
+		// 키보드 두 수의 입력
+		// 화면에 두수의 합 출력
+		Scanner scan = new Scanner(System.in);//키보드 system
+		String one = scan.nextLine();
 		String[] a = one.split(",");
-		
+
 		int i = Integer.parseInt(a[0]);
- 		int j = Integer.parseInt(a[1]);
- 		
- 		System.out.println(i + j);
- 		
- 		int value1 = scan.nextInt();
- 		int value2 = scan.nextInt();
- 		
- 		System.out.println(value1 + value2);
+		int j = Integer.parseInt(a[1]);
+
+		System.out.println(i + j);
+
+		int value1 = scan.nextInt();
+		int value2 = scan.nextInt();
+
+		System.out.println(value1 + value2);
+		System.out.printf("%d 와 %d의 합은 %d 입니다",value1,value2,value1+value2);
+		scan.close();
 	}
 }
