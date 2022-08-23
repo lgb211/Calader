7d67e3795bd2ab6355b7d8fb06d2c69471f18b0d
lgb211 <rkdqls0122@naver.com>
2022-08-23 13:31:09 +0900

    Weekday input

--- src/PrintCalander.java
+++ src/PrintCalander.java
@@ -20,16 +20,43 @@ public class PrintCalander {
 		}
 	}
 
-	public static void print(int year,int month) {
+	public static int getweekday(String weekday) {
+		switch(weekday) {
+		case "월":
+			return 1;
+		case "화":
+			return 2;
+		case "수":
+			return 3;
+		case "목":
+			return 4;
+		case "금":
+			return 5;
+		case "토":
+			return 6;
+		case "일":
+			return 0;
+		default :
+			return 0;
+		}
+		
+			
+	}
+	
+	public static void print(int year,int month,String weekday) {
+
 		int j = getmaxDayOfMonth(year,month);
-		System.out.println(j);
+		int m = getweekday(weekday);
 		System.out.println(">");
-		System.out.println(" 일  월   화   수   목   금  토");
+		System.out.println("  일   월   화   수   목   금  토");
 		System.out.println("--------------------");
+		for(int n=1;n<m*3+1;n++) {
+			System.out.printf(" ");
+		}
 		if(MAX_DAYS[month] !=0) {
 			for (int i = 1; i <= j; i++) {
 				System.out.printf("%3d", i);
-				if (i % 7 == 0)
+				if ((i+m) % 7 == 0)
 					System.out.println();
 			}
 		}else {
@@ -44,24 +71,33 @@ public class PrintCalander {
 	}
 
 	public static void main(String[] args) {
+	
 		Scanner scan = new Scanner(System.in);
 		System.out.println("년도를 입력하세요");
 		int year = scan.nextInt();
 		System.out.println("월을 입력하세요");
 		int month = scan.nextInt();
+		System.out.println("시작 요일을 입력하세요");
+		System.out.println(" ");
+		String weekday = scan.next();
 		while (true) {
 			if (month == -1) {
 				System.out.println("bye~");
 				break;
-			} else {
-				print(year,month);
+			} else {				
+				print(year,month,weekday);
 				System.out.println();
 				System.out.println("년도를 입력하세요");
 				year = scan.nextInt();
 				System.out.println("월을 입력하세요");
 				month = scan.nextInt();
+				System.out.println("시작 요일을 입력하세요");
+				System.out.println(" ");
+				weekday = scan.next();
 			}
 		}
+		
+		scan.close();
 	}
 
 }
